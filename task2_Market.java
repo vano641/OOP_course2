6ecacd2d5ac206000855ab723e31ca266c3d155a
Ivan Mysovskii <vano641@mail.ru>
2024-10-21 13:52:30 +0300

    Класс содержит список Акторов и реализует  интерфейсы Очереди и Магазина

---
+++ task2_Market.java
@@ -0,0 +1,42 @@
+import java.util.List;
+
+public class task2_Market implements task2_MarketBehaviour, task2_QueueBehaviour{
+
+    private List<task2_Actor> queue; // список Акторов
+
+    @Override
+    public void takeInQueue(task2_Actor actor) {
+      
+    }
+
+    @Override
+    public void takeOrders() {
+        
+    }
+
+    @Override
+    public void giveOrders() {
+        
+    }
+
+    @Override
+    public void releaseFromQueue() {
+       
+    }
+
+    @Override
+    public void acceptToMarket(task2_Actor actor) {
+        
+    }
+
+    @Override
+    public void releaseFromMarket(List<task2_Actor> actorList) {
+        
+    }
+
+    @Override
+    public void update() {
+        
+    }
+    
+}
