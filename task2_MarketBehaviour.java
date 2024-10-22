580a4694e0bd02746ebc37e3e29ae16e35f5374c
Ivan Mysovskii <vano641@mail.ru>
2024-10-22 14:28:54 +0300

    Интерфейс магазина

--- task2_MarketBehaviour.java
+++ task2_MarketBehaviour.java
@@ -1,7 +1,7 @@
 import java.util.List;
 public interface task2_MarketBehaviour { 
     
-    void acceptToMarket(task2_Actor actor); // добавление посетителя в магазин
+    void acceptToMarket(); // добавление посетителя в магазин
 
     void releaseFromMarket(List<task2_Actor> actorList); // метод выхода из магазина
 
